6aacf45bfe5b18c0716774ac6b4de038dbfedffa
scethen <Kyle.Tran@selu.edu>
2023-02-08 16:01:35 -0600

    Dto added

---
+++ SP23.P02.Web/Features/User/CreateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SP23.P02.Web.Features.User
+{
+    public class CreateUserDto
+    {
+        public string UserName { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
---
+++ SP23.P02.Web/Features/User/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SP23.P02.Web.Features.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string[] Role { get; set; }
+    }
+}
